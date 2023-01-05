f5d541acb17082309ad40c3e92dc67ef23b00292
Renzo Quero <renzoquero@gmail.com>
2023-01-05 09:46:15 -0300

    commit inicial

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+print("Hello")
+print("World!")
+name = input ("Cual es tu nombre?")
+print("Hola")
+print(name)
+x = 532 + 12
+
+print ("Ya hice matematicas")
+
+print (input ("Ingrese su nombre: "))
+print (input ("Ingrese su apellido: "))
